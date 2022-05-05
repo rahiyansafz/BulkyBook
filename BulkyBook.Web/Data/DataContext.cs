103e728c03c2712a822a0e125d9cbff1114b4d3f
rahiyansafz <rahiyansafin@gmail.com>
2022-05-05 20:58:17 +0600

    Add project files.

---
+++ BulkyBook.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBook.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Web.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set; } = null!;
+}
---
+++ BulkyBook.Web/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Web.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
