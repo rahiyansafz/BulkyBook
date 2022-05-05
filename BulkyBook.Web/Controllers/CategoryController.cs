386fbb28788ea428ba8f27d940f22207a8f0ba1b
rahiyansafz <rahiyansafin@gmail.com>
2022-05-05 23:42:49 +0600

    Category Controller Retrieve All Category Implemented

---
+++ BulkyBook.Web/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Web.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly DataContext _dataContext;
+
+    public CategoryController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+    public IActionResult Index()
+    {
+        var objCategoryList = _dataContext.Categories.ToList();
+        return View();
+    }
+}
