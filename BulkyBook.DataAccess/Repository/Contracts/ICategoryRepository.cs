bd60a299bc49283bffdd313d994240b19af80ea2
rahiyansafz <rahiyansafin@gmail.com>
2022-05-10 19:30:44 +0600

    Category Repository Contract Implemented

---
+++ BulkyBook.DataAccess/Repository/Contracts/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BulkyBook.Models.Models;
+
+namespace BulkyBook.DataAccess.Repository.Contracts;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category category);
+    void Save();
+}
