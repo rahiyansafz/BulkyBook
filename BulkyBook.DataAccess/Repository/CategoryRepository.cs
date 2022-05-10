32235511785c4a3537697df0ee196cf765a677ba
rahiyansafz <rahiyansafin@gmail.com>
2022-05-10 19:39:04 +0600

    Category Repository Implemented

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.Contracts;
+using BulkyBook.Models.Models;
+
+namespace BulkyBook.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private readonly DataContext _context;
+
+    public CategoryRepository(DataContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async void Save()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Update(Category category)
+    {
+        _context.Categories.Update(category);
+    }
+}
