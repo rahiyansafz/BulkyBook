98675fd5a858fc11ff4c7b5dd4b18a9b8bf02c9b
rahiyansafz <rahiyansafin@gmail.com>
2022-05-11 20:09:18 +0600

    Product Model Implemented (In-Progress)

---
+++ BulkyBook.Models/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    [Required]
+    public string ISBN { get; set; } = string.Empty;
+    [Required]
+    public string Author { get; set; } = string.Empty;
+    [Required]
+    [Range(1, 10000)]
+    public double ListPrice { get; set; }
+}
